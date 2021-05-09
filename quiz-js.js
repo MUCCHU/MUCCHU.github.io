797d66e5cb37e7b83c89d845d6036e89e3eab56c
unknown <harshwasnik160602@gmail.com>
2021-05-09 15:10:27 +0530

    New Beginning

---
+++ quiz-js.js
@@ -0,0 +1,11 @@
+// var opt=document.getElementsByClassName("options");
+// opt[0].style.color="red";
+// var i;
+// for(i=0;i<opt.length;i++)
+// {
+//     opt[i].addEventListener("click", function(){
+//         this.parentElement.style.bcakgroundColor="green";
+//         this.parentElement.style.color="green";
+//     } )
+// }
+document.getElementsByClassName("op1").style.color="red";
