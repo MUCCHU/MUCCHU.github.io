a0bbaa94e0f7ef196a5027caa470927361423f56
unknown <harshwasnik160602@gmail.com>
2021-06-12 14:56:15 +0530

    Restart Game fixed, background changed

--- ktjWorkshop/A2/Javascript/mainBrain.js
+++ ktjWorkshop/A2/Javascript/mainBrain.js
@@ -1,230 +1,196 @@
 var r = 2, c = 2;
 var grid;
-function createArr()
-{
-    grid=new Array(r);
-    for(var i=0;i<r;i++)
-    grid[i]=new Array(c);
+function createArr() {
+    grid = new Array(r);
+    for (var i = 0; i < r; i++)
+        grid[i] = new Array(c);
     console.log("created array");
 }
 //Function to get random numbers such that no number is repeated
-class cell{
-    constructor(serialNumber, rowNo, colNo, )
-    {
+class cell {
+    constructor(serialNumber, rowNo, colNo,) {
         console.log("created a cell ")
         this.srno = serialNumber;
         this.rno = rowNo;
         this.cno = colNo;
-        if(this.srno==r*c)
-        this.isEmpty=true;
+        if (this.srno == r * c)
+            this.isEmpty = true;
         else
-        this.isEmpty=false;
-        this.neighbours=[];
+            this.isEmpty = false;
+        this.neighbours = [];
     }
     createCell() {
-        var parentRow = document.getElementsByClassName("row"+this.rno.toString())[0];
+        var parentRow = document.getElementsByClassName("row" + this.rno.toString())[0];
         var cellOuter = document.createElement("div");
         var newCell = document.createElement("div");
         cellOuter.appendChild(newCell);
         parentRow.appendChild(cellOuter);
-        newCell.innerHTML = "cell "+this.srno.toString();
-        if(this.isEmpty)
-        newCell.innerHTML="Empty";
-        newCell.className = "cell cell"+this.srno.toString();
+        newCell.innerHTML = this.srno.toString();
+        if (this.isEmpty)
+            newCell.innerHTML = "Empty";
+        newCell.className = "cell cell" + this.srno.toString();
         cellOuter.className = "CellOuterB";
-        newCell.id= "r"+this.rno.toString()+"c"+this.cno.toString();
+        newCell.id = "r" + this.rno.toString() + "c" + this.cno.toString();
         this.formatEmptyCell();
         newCell.addEventListener("click", cellClicked);
     }
-    formatEmptyCell()
-    {
-        if(this.isEmpty)
-        {
-            var newCell = document.getElementById("r"+this.rno.toString()+"c"+this.cno.toString());
+    formatEmptyCell() {
+        if (this.isEmpty) {
+            var newCell = document.getElementById("r" + this.rno.toString() + "c" + this.cno.toString());
             console.log(newCell);
-            newCell.innerHTML="";
-            newCell.style.width="0px";
-            newCell.style.height="0px";
+            newCell.className += " empty"
+            newCell.innerHTML = "";
+            newCell.style.width = "0px";
+            newCell.style.height = "0px";
         }
     }
-    formatNonEmptyCell()
-    {
+    formatNonEmptyCell() {
         console.log("fnEc executed");
-        if(this.isEmpty==false)
-        {
+        if (this.isEmpty == false) {
             console.log("Formatting non empty cell");
-            var newCell = document.getElementById("r"+this.rno.toString()+"c"+this.cno.toString());
-            newCell.style.width="80px";
-            newCell.style.height="80px";
-            newCell.innerHTML="Cell"+this.srno.toString();
+            var newCell = document.getElementById("r" + this.rno.toString() + "c" + this.cno.toString());
+            newCell.classList.remove("empty");
+            newCell.style.width = "80px";
+            newCell.style.height = "80px";
+            newCell.innerHTML = this.srno.toString();
         }
     }
-    computeNgb(){
+    computeNgb() {
 
-        if(this.rno>0)
-        {
-            this.neighbours.push(grid[this.rno-1][this.cno]);
+        if (this.rno > 0) {
+            this.neighbours.push(grid[this.rno - 1][this.cno]);
         }
-        if(this.rno<r-1)
-        {
-            this.neighbours.push(grid[this.rno+1][this.cno]);
+        if (this.rno < r - 1) {
+            this.neighbours.push(grid[this.rno + 1][this.cno]);
         }
-        if(this.cno>0)
-        {
-            this.neighbours.push(grid[this.rno][this.cno-1]);
+        if (this.cno > 0) {
+            this.neighbours.push(grid[this.rno][this.cno - 1]);
         }
-        if(this.cno<c-1)
-        {
-            this.neighbours.push(grid[this.rno][this.cno+1]);
+        if (this.cno < c - 1) {
+            this.neighbours.push(grid[this.rno][this.cno + 1]);
         }
         console.log(this.neighbours);
     }
-    ngbEmptyIndex()
-    {
+    ngbEmptyIndex() {
         this.computeNgb();
-        var foundEcell=false;
+        var foundEcell = false;
         var i;
-        for(i=0;i<this.neighbours.length;i++)
-        {
-            if(this.neighbours[i].isEmpty==true)
-            {
-                foundEcell=true;
+        for (i = 0; i < this.neighbours.length; i++) {
+            if (this.neighbours[i].isEmpty == true) {
+                foundEcell = true;
                 break;
             }
         }
-        if(foundEcell)
-        return i;
+        if (foundEcell)
+            return i;
         else
-        return -1;
+            return -1;
     }
-    swap(coordinateOfEmptyngb)
-    {
+    swap(coordinateOfEmptyngb) {
         var emptyCell = this.neighbours[coordinateOfEmptyngb];
         emptyCell.isEmpty = false;
         emptyCell.srno = this.srno;
         emptyCell.formatNonEmptyCell();
         this.isEmpty = true;
-        this.srno = r*c;
+        this.srno = r * c;
         this.formatEmptyCell();
 
     }
 }
 function checkIfWon() {
     console.log("Checking if winning position achieved");
-    var k=1;
-    var won=true;
-    for(var i=0;i<r;i++)
-    {
-        for(var j=0;j<c;j++)
-        {
-            if(grid[i][j].srno!=k)
-            {
-                won=false;
-                var highlightedcell = document.getElementById("r"+i.toString()+"c"+j.toString());
-                 highlightedcell.style.backgroundColor = "chartreuse";
+    var k = 1;
+    var won = true;
+    for (var i = 0; i < r; i++) {
+        for (var j = 0; j < c; j++) {
+            if (grid[i][j].srno != k) {
+                won = false;
+                var highlightedcell = document.getElementById("r" + i.toString() + "c" + j.toString());
+                highlightedcell.style.backgroundColor = "chartreuse";
             }
-            else
-            {
-                var highlightedcell = document.getElementById("r"+i.toString()+"c"+j.toString());
-                    highlightedcell.style.backgroundColor = "Red";
+            else {
+                var highlightedcell = document.getElementById("r" + i.toString() + "c" + j.toString());
+                highlightedcell.style.backgroundColor = "Red";
             }
             k++;
         }
     }
     return (won);
-    
+
 }
 function shuffle(array) {
-    var currentIndex = array.length,  randomIndex;
-  
+    var currentIndex = array.length, randomIndex;
+
     // While there remain elements to shuffle...
     while (0 !== currentIndex) {
-  
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
     }
-  
+
     return array;
-  }
-  function cellClicked()
-    {
-        var ClickedRno = this.id.slice(this.id.indexOf("r")+1, this.id.indexOf("c"));
-        var ClickedCno = this.id.slice(this.id.indexOf("c")+1);
-        console.log(ClickedRno);
-        console.log(ClickedCno);
-        console.log("cell click detected");
-        var eIndex=grid[ClickedRno][ClickedCno].ngbEmptyIndex();
-        if(eIndex!=-1){
-            grid[ClickedRno][ClickedCno].swap(eIndex);
-            console.log("swap possible");
-            if(checkIfWon())
-            {
-                console.log("Winner!!");
-                document.getElementById("WinningMessage").innerHTML="WON";
-                document.getElementById('gridSpace').style.pointerEvents = 'none';
-                document.getElementById("tryAgainButton").style.display="block";
-            }
+}
+function cellClicked() {
+    var ClickedRno = this.id.slice(this.id.indexOf("r") + 1, this.id.indexOf("c"));
+    var ClickedCno = this.id.slice(this.id.indexOf("c") + 1);
+    console.log(ClickedRno);
+    console.log(ClickedCno);
+    console.log("cell click detected");
+    var eIndex = grid[ClickedRno][ClickedCno].ngbEmptyIndex();
+    if (eIndex != -1) {
+        grid[ClickedRno][ClickedCno].swap(eIndex);
+        console.log("swap possible");
+        if (checkIfWon()) {
+            console.log("Winner!!");
+            document.getElementById("WinningMessage").style.display = "block";
+            document.getElementById("WinningMessage").innerHTML = "WON";
+            document.getElementById('gridSpace').style.pointerEvents = 'none';
+            document.getElementById("tryAgainButton").style.display = "block";
         }
     }
-function generateGrid()
-{
+}
+function generateGrid() {
     createArr();
-    document.getElementById("tryAgainButton").style.display="none";
+    document.getElementById("tryAgainButton").style.display = "none";
+    document.getElementById("WinningMessage").style.display = "none";
     document.getElementById('gridSpace').style.pointerEvents = 'auto';
     var container = document.getElementById("gridSpace");
-    container.innerHTML="";
-    var k=0;
-        var ar=[];
-        for(var i=1;i<=c*r;i++)
+    container.innerHTML = "";
+    var k = 0;
+    var ar = [];
+    for (var i = 1; i <= c * r; i++)
         ar.push(i);
-        shuffle(ar);
-    for(var i=0;i<r;i++)
-    {
+    shuffle(ar);
+    for (var i = 0; i < r; i++) {
         var newR = document.createElement("div");
         container.appendChild(newR);
-        newR.className = "row row"+i.toString(); 
-        for(var j=0;j<c;j++)
-        {
-            grid[i][j]= new cell(ar[k], i, j);
+        newR.className = "row row" + i.toString();
+        for (var j = 0; j < c; j++) {
+            grid[i][j] = new cell(ar[k], i, j);
             grid[i][j].createCell();
             k++;
         }
     }
 }
 DisplayRestart();
-function DisplayRestart()
-{
-    document.getElementById("numberOfRows").value=4;
-    document.getElementById("numberOfColumns").value=4;
-    rowinp.value=4;
-    colinp.value=4;
+function DisplayRestart() {
+    document.getElementsByClassName("semiTrans")[0].style.display = "block";
+    document.getElementById("numberOfRows").value = 4;
+    document.getElementById("numberOfColumns").value = 4;
+    rowinp.value = 4;
+    colinp.value = 4;
 }
 function restartGame() {
     var rowinp = document.getElementById("numberOfRows").value;
     var colinp = document.getElementById("numberOfColumns").value;
-    r=rowinp;
-    c=colinp;
+    r = rowinp;
+    c = colinp;
     console.log(document.getElementsByClassName("semiTrans")[0]);
-    document.getElementsByClassName("semiTrans")[0].style.display="none";
+    document.getElementsByClassName("semiTrans")[0].style.display = "none";
     generateGrid();
 }
-//Code to test animation
-// var itemtomove = document.getElementsByClassName("toMove")[0].style.position = "absolute";
-// var pos=0;
-// var idiot = setInterval(frame, 5); 
-// function frame() {
-//     if(pos==256)
-//     clearInterval(idiot);
-//     else
-//     {
-//         pos++;
-//         itemtomove.style.bottom=pos+"px";
-//     }
-// }
-
-// console.log(itemtomove);
