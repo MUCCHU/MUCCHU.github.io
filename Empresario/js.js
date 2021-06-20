8738789b19d866cd6e42c2aa6aab4b00a1c7d1c5
unknown <harshwasnik160602@gmail.com>
2021-06-20 21:47:29 +0530

    register modal added

--- Empresario/js.js
+++ Empresario/js.js
@@ -1,6 +1,12 @@
 function emPdisplaYlogiNforM() {
-    document.getElementsByClassName("emP-bacKdroP")[0].style.display = "flex";
+    document.getElementsByClassName("emP-bacKdroP-logiN")[0].style.display = "flex";
 }
 function emPhidElogiNforM() {
-    document.getElementsByClassName("emP-bacKdroP")[0].style.display = "none";
+    document.getElementsByClassName("emP-bacKdroP-logiN")[0].style.display = "none";
+}
+function emPdisplaYregistratioNforM() {
+    document.getElementsByClassName("emP-bacKdroP-registeR")[0].style.display = "flex";
+}
+function emPhidEregistratioNforM() {
+    document.getElementsByClassName("emP-bacKdroP-registeR")[0].style.display = "none";
 }
