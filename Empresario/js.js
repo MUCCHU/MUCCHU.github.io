4a06253418fa3a615e09807fae8e975109e62335
unknown <harshwasnik160602@gmail.com>
2021-06-20 17:37:39 +0530

    modal created

---
+++ Empresario/js.js
@@ -0,0 +1,6 @@
+function emPdisplaYlogiNforM() {
+    document.getElementsByClassName("emP-bacKdroP")[0].style.display = "flex";
+}
+function emPhidElogiNforM() {
+    document.getElementsByClassName("emP-bacKdroP")[0].style.display = "none";
+}
